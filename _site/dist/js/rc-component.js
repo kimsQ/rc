34643d4811dc0784bdb73120325f7cfffa0d6cce
Gitaek kwon <break@kimsq.com>
2016-06-02 10:07:45 +0900

    jekyll 기본 구조 추가

---
+++ _site/dist/js/rc-component.js
@@ -0,0 +1,11 @@
+/*!
+ * =====================================================
+ * Ratchet Plus v0.5 (https://github.com/kimsq/rc)
+ * =====================================================
+ * inspired by @twbs's bootstrap & ratchet
+ * Copyright 2016 redblock inc.
+ * Author kiere (kiere@kimsq.com)
+ * Licensed under MIT.
+ * =====================================================
+ */
+
