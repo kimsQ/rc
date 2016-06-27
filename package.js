191b542fa2af162862d108ef273a0ea848913e7e
Gitaek kwon <break@kimsq.com>
2016-06-27 12:19:50 +0900

    Package managers 메타정보 수정

--- package.js
+++ package.js
@@ -3,17 +3,17 @@
 /* global Package:true */
 
 Package.describe({
-  name: 'twbs:bootstrap',  // http://atmospherejs.com/twbs/bootstrap
-  summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '4.0.0-alpha.2',
-  git: 'https://github.com/twbs/bootstrap.git'
+  name: 'kimsQ:ratchet-plus',  // http://atmospherejs.com/twbs/bootstrap
+  summary: 'The most popular front-end framework for developing responsive, mobile only projects on the web.',
+  version: '1.0.0-alpha.1',
+  git: 'https://github.com/kimsQ/rc.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
   api.use('jquery', 'client');
   api.addFiles([
-    'dist/css/bootstrap.css',
-    'dist/js/bootstrap.js'
+    'dist/css/ratchet-plus.css',
+    'dist/js/ratchet-plus.js'
   ], 'client');
 });
