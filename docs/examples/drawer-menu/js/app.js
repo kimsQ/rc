5b913ad88e55d0de27e2fe6b83373f94559ce685
Gitaek kwon <break@kimsq.com>
2016-07-27 18:16:53 +0900

    드로어 예제 개선
    드로어 아이템 탭시에 드로어 숨김 처리 추가

--- docs/examples/drawer-menu/js/app.js
+++ docs/examples/drawer-menu/js/app.js
@@ -1,4 +1,11 @@
 // Your app's javascript, make it your own
+
 $(document).ready(function(){
   RC_initDrawer();
+
+  // 드로어 아이템 탭시에 드로어 숨기기 처리
+  $('.snap-drawer .table-view-cell').tap(function() {
+    $('#myDrawer').drawer('hide')
+  });
+
 });
