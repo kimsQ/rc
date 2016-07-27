b448c722aa43a8b26480017c0da60349e14ef668
Gitaek kwon <break@kimsq.com>
2016-07-27 18:34:44 +0900

    드로어 아이템 tap 이벤트 시에 드로어 닫기처리

--- docs/examples/drawer-menu/js/app.js
+++ docs/examples/drawer-menu/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   RC_initDrawer();
 
   // 드로어 아이템 탭시에 드로어 숨기기 처리
-  $('.snap-drawer .table-view-cell').click(function() {
+  $('.snap-drawer .table-view-cell').tap(function() {
     $('#myDrawer').drawer('hide')
   });
 
